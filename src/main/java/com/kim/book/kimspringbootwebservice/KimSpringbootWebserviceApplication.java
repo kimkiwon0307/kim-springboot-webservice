40d4a8195f0de195744f1be4b9bbf88aee79a6f3
kkwkkj@naver.com <kkwkkj@naver.com>
2022-08-15 01:07:45 +0900

    Initial commit

---
+++ src/main/java/com/kim/book/kimspringbootwebservice/KimSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kim.book.kimspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimSpringbootWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kim/book/kimspringbootwebservice/KimSpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.book.kimspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimSpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
